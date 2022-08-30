c0734927bc5332e4b98f7536e0314d5146fdf9d5
binettexyz <46168797+binettexyz@users.noreply.github.com>
2022-08-30 13:05:50 -0400

    .

--- config.h
+++ config.h
@@ -13,10 +13,10 @@ static const struct arg args[] = {
 	/* function format          argument */
 	{run_command, "[%s - ",  "/home/binette/.local/bin/statusbar/sb-nettraf" },
 	{run_command, "%s] ",  "/home/binette/.local/bin/statusbar/sb-internet" },
-//	{cpu_perc, "[ %s%% | ", NULL },
+	{cpu_perc, "[ %s%% | ", NULL },
 	{temp, "%s°c] ",        "/sys/class/thermal/thermal_zone0/temp" },
-	{run_command, "[%s] ",  "/home/binette/.local/bin/statusbar/sb-volume" },
-	{run_command, "[%s] | ",  "/home/binette/.local/bin/statusbar/sb-battery" },
+//	{run_command, "[%s] ",  "/home/binette/.local/bin/statusbar/sb-volume" },
+//	{run_command, "[%s] | ",  "/home/binette/.local/bin/statusbar/sb-battery" },
 	{run_command, "%s",  "date '+[%b %d (%a) %Y - %I:%M%p] '" },
 	//{disk_used, "[STO] %s/", "/home" },
 	//{disk_total, "%s | ", "/home" },*/
